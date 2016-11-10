17219b98d5af4352589231cc9c7b2008ee454536
u4bi <u4bi@u4bi.com>
2016-11-10 16:13:57 +0000

    new & delete void NewDeleteExample

--- main.cpp
+++ main.cpp
@@ -20,6 +20,36 @@ using namespace std;
 void func(int a){ cout << "int a : " << a << endl; }
 void func(int a, int b){ cout << "int a : " << a << " int b : " << b << endl; }
 
+void NewDeleteExample(){
+    int len, total=0;
+    int * score;
+    int i;
+    
+    cout << "학생수 입력 : ";
+    cin >> len;
+    
+    score = new int[len];
+    /*  new 연산이 등장 len에 5라는 값이 들어왔다고 가정하면
+        길이가 5인 int형 배열을 동적 할당함
+    */
+    
+    for(i=0; i<len; i++){
+        cout << i+1 << "번 학생의 점수: ";
+        cin >> score[i];
+        
+        total += score[i];
+    }
+    
+    cout << "모든 학생의 평균: " << total/len << endl;
+    delete []score;
+    /*  동적으로 할당된 int형 배열이기 때문에 []score로 해제해주어야 함
+    
+    delete와 delete[]는 서로 다른 기능을 하는 연산자임.
+    
+    delete 연산자를 사용하면 단일 객체에 대한 소멸자를 호출하고 메모리를 반환 함
+    그러나 delete[] 연산자를 사용하면 각 요소마다 소멸자를 호출하여 메모리 반환 함.
+    */
+}
 int main() {
     
     int * ptr1 = new int; // 객체를 동적 할당
@@ -41,6 +71,8 @@ int main() {
     A::Add();
     B::Add();
     
+    NewDeleteExample();
+        
     char name[10];
     int age;
     std::cin>>name>>age;
