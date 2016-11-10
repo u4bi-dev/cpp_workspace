a58a30812863400a1792adf1b620a16c19ca7633
u4bi <u4bi@u4bi.com>
2016-11-10 10:06:32 +0000

    std cin

--- main.cpp
+++ main.cpp
@@ -10,6 +10,16 @@
     
 */
 int main() {
+    char name[10];
+    int age;
+    
+    // cin
+    std::cin>>name>>age;
+    /*  키보드로 전달받은 값을 >>으로 전달
+        데이터 입력 기준 스페이스바, 탭 엔터키같은 공백에 의해 나뉨. */
+    
+    // cout
+    std::cout << "내 닉네임은 " << name << "이며 " << age << "살입니다." <<std::endl;
     std::cout << "u4bi c++ workspace" <<std::endl;
     return 0;
 }
