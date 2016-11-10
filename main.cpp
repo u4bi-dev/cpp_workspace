0db25db799b5ec92bdd4594b3351bc3a4427adf9
u4bi <u4bi@u4bi.com>
2016-11-10 10:05:46 +0000

    std cout

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream> 
-
+/* c++ 기본 입출력 함수인 cin, cout
+    cout : printf와 흡사
+    
+    cout 사용 위해 앞에 std:: 붙어야함.
+    << >> : 시프트 연산자라 불림.
+    << : 왼쪽으로 전달 >> : 오른쪽으로 전달
+    
+    std::endl : c언어에서 ₩n 개행과 동일 줄바꿔줌
+    
+*/
 int main() {
     std::cout << "u4bi c++ workspace" <<std::endl;
     return 0;
