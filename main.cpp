bc3ac61769cb0fc49af0411941090ff24bb02177
u4bi <u4bi@u4bi.com>
2016-11-10 16:56:11 +0000

    struct

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream> 
 
-/*  힙 영역에 메모리 공간을 할당할 수 있게 도와주는 함수
-    new 함수로 메모리를 동적 할당 delete로 동적 할당한 메모리를 해제함.
+/*  구조체를 사용하여 관련있는 변수들끼리 묶어 손쉽게 관리할 수 있음
+    C++ 구조체
 */
 
 namespace A { void Add() { printf("A의 Add() 호출 \n");}}
@@ -17,48 +17,43 @@ using namespace u4bi;
 using github::win;
 
 using namespace std;
+
+struct player{
+    int id;
+    char name[24];
+    float exp;
+};
+
 void func(int a){ cout << "int a : " << a << endl; }
 void func(int a, int b){ cout << "int a : " << a << " int b : " << b << endl; }
 
 void NewDeleteExample(){
-    int len, total=0;
-    int * score;
-    int i;
-    
+    int len, total=0; int * score; int i;    
     cout << "학생수 입력 : ";
     cin >> len;
     
     score = new int[len];
-    /*  new 연산이 등장 len에 5라는 값이 들어왔다고 가정하면
-        길이가 5인 int형 배열을 동적 할당함
-    */
     
     for(i=0; i<len; i++){
         cout << i+1 << "번 학생의 점수: ";
         cin >> score[i];
-        
         total += score[i];
     }
-    
     cout << "모든 학생의 평균: " << total/len << endl;
     delete []score;
-    /*  동적으로 할당된 int형 배열이기 때문에 []score로 해제해주어야 함
-    
-    delete와 delete[]는 서로 다른 기능을 하는 연산자임.
-    
-    delete 연산자를 사용하면 단일 객체에 대한 소멸자를 호출하고 메모리를 반환 함
-    그러나 delete[] 연산자를 사용하면 각 요소마다 소멸자를 호출하여 메모리 반환 함.
-    */
 }
+
 int main() {
     
-    int * ptr1 = new int; // 객체를 동적 할당
-    int * ptr2 = new int[10]; // 길이가 10인 객체의 배열을 동적 할당
-    delete ptr1; // 동적 할당된 객체를 소멸 - 3행
-    delete []ptr2; // 동적 할당된 객체의 배열을 소멸 - 4행
-    /*  동적 할당된 객체의 배열을 소멸시킬때는 4행처럼 작성해야 함
-        만약 3행처럼 소멸시킬다면 메모리 누수가 발생한다고 함.
-    */
+    player p = {1, "u4bi", 67.5};
+    cout << "번호 : " <<p.id << endl;
+    cout << "이름 : " <<p.name << endl;
+    cout << "경험 : " <<p.exp << endl;
+    
+    int * ptr1 = new int;
+    int * ptr2 = new int[10];
+    delete ptr1;
+    delete []ptr2;
     
     func(4);
     func(5, 6);
