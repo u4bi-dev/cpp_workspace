8f72704ff90e6aac0d0c1cc3a460f91761903899
u4bi <u4bi@u4bi.com>
2016-12-01 17:09:03 +0000

    Bike header - Bike.cpp showBikeState

--- Bike.cpp
+++ Bike.cpp
@@ -3,8 +3,14 @@
 #include "Bike.h"
 using namespace std;
 
+/* 이닛 유저이름, 초기 주입될 기름*/
 void Bike::initPlayer(char *NAME, int fuel){
     strcpy(playerName, NAME);
     fuelGauge= fuel;
     speedGauge=0;
+}
+
+/* 현재 바이크 상태보기*/
+void Bike::showBikeState(){
+  cout << "이름 : " << playerName << " 현재 연료 : " << fuelGauge << " 현재 속도 : " << speedGauge << endl;
 }
