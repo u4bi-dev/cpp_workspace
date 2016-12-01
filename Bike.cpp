eed25b6fd10dbdce50b3098923042f4aad5984ae
u4bi <u4bi@u4bi.com>
2016-12-01 17:11:36 +0000

    Bike header - Bike.cpp bikeAccel 1

--- Bike.cpp
+++ Bike.cpp
@@ -14,3 +14,9 @@ void Bike::initPlayer(char *NAME, int fuel){
 void Bike::showBikeState(){
   cout << "이름 : " << playerName << " 현재 연료 : " << fuelGauge << " 현재 속도 : " << speedGauge << endl;
 }
+
+void Bike::bikeAccel(){
+  cout << "부아아아앙" << endl;
+  if(fuelGauge <= 0) return; /* 0이거나 0이 더 높으면 리턴*/
+  else fuelGauge -= CAR_CONST::FUEL_STEP; /*아니면 상수에 설정된 만큼 차감하기*/
+}
