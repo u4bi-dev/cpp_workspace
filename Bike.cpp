cdfb8cc2fa4523d98647e7d9fea2b765471aa1a3
u4bi <u4bi@u4bi.com>
2016-12-01 17:06:04 +0000

    Bike header - Bike.cpp initPlayer

--- Bike.cpp
+++ Bike.cpp
@@ -2,3 +2,9 @@
 #include <cstring>
 #include "Bike.h"
 using namespace std;
+
+void Bike::initPlayer(char *NAME, int fuel){
+    strcpy(playerName, NAME);
+    fuelGauge= fuel;
+    speedGauge=0;
+}
